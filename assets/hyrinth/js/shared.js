614a9a672ad19c3e351c0c1c432e23ecbf45971b
nerotvlive <daniel.niesmann@outlook.com>
2025-12-06 12:03:24 +0100

    test shared.js

--- assets/hyrinth/js/shared.js
+++ assets/hyrinth/js/shared.js
@@ -1,5 +1,5 @@
 if(window.location.host.toLowerCase().includes("hyrinth.com")&&window.location.href.toLowerCase().includes(".html")) {
-    window.location.href = window.location.href.replace(".html", "").replace("index.html", "");
+    window.location.href = window.location.href.replace("index.html", "").replace(".html", "");
 }
 if(window.location.host.toLowerCase().includes("hyrinth.com")&&window.location.href.toLowerCase().includes("index")) {
     window.location.href = window.location.href.replace("index", "");
