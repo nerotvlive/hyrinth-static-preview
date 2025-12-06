d85f73e8ecdd0b441d7de07ba4ec2bf852a19197
nerotvlive <daniel.niesmann@outlook.com>
2025-12-06 12:08:57 +0100

    optimize automatic redirect behavior

--- assets/hyrinth/js/shared.js
+++ assets/hyrinth/js/shared.js
@@ -1,6 +1,5 @@
-if(window.location.host.toLowerCase().includes("hyrinth.com")&&window.location.href.toLowerCase().includes(".html")) {
-    window.location.href = window.location.href.replace("index.html", "").replace(".html", "");
-}
-if(window.location.host.toLowerCase().includes("hyrinth.com")&&window.location.href.toLowerCase().includes("index")) {
-    window.location.href = window.location.href.replace("index", "");
+if(window.location.host.toLowerCase().includes("hyrinth.com")) {
+    if(window.location.href.toLowerCase().includes(".html")||window.location.href.toLowerCase().includes("/index")) {
+        window.location.href = window.location.href.replace("/index.html", "").replace("/index", "").replace(".html", "");
+    }
 }
